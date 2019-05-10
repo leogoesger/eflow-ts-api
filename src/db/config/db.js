66896602bf78495bf4260489672d30c72a6088d9
Madhav Parekh <madhavparekh@gmail.com>
2019-05-10 10:21:26 -0700

    FF-410: revert and minor fix for db

--- src/db/config/db.js
+++ src/db/config/db.js
@@ -1,44 +1,44 @@
 module.exports = {
   development: {
-    username: 'postgres',
+    username: "postgres",
     password: null,
-    database: 'eflow_development',
-    host: '127.0.0.1',
+    database: "eflow_development",
+    host: "127.0.0.1",
     post: 5432,
-    dialect: 'postgres',
+    dialect: "postgres",
     logging: false,
     pool: {
       max: 30,
       min: 0,
       idle: 1000000,
-      acquire: 1000000,
+      acquire: 1000000
     },
-    operatorsAliases: Op,
+    operatorsAliases: true
   },
   test: {
-    username: 'postgres',
+    username: "postgres",
     password: null,
-    database: 'eflow_test',
-    host: '127.0.0.1',
+    database: "eflow_test",
+    host: "127.0.0.1",
     post: 5432,
-    dialect: 'postgres',
+    dialect: "postgres",
     logging: false,
-    operatorsAliases: Op,
+    operatorsAliases: true
   },
   production: {
-    username: 'production',
-    password: 'production123',
-    database: 'eflow_production',
-    host: '127.0.0.1',
+    username: "production",
+    password: "production123",
+    database: "eflow_production",
+    host: "127.0.0.1",
     post: 5432,
-    dialect: 'postgres',
+    dialect: "postgres",
     logging: false,
     pool: {
       max: 30,
       min: 0,
       idle: 1000000,
-      acquire: 1000000,
+      acquire: 1000000
     },
-    operatorsAliases: Op,
-  },
+    operatorsAliases: true
+  }
 };
